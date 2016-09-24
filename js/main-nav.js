fa3722a6892544c6a9e23c8f4fb07aba16d43d2d
Zillah <eclypsis@mail.ru>
2016-09-24 17:56:26 +0300

    js

--- js/main-nav.js
+++ js/main-nav.js
@@ -4,11 +4,13 @@ var navToggle = document.querySelector('.main-nav__toggle');
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
+  if (navToggle.classList.contains('main-nav--closed')) {
+    navToggle.classList.remove('main-nav--closed');
+    navToggle.classList.add('main-nav__toggle--opened');
     navMain.classList.add('main-nav--opened');
     } else {
-        navMain.classList.add('main-nav--closed');
+        navToggle.classList.add('main-nav--closed');
+        navToggle.classList.remove('main-nav__toggle--opened');
         navMain.classList.remove('main-nav--opened');
       }
 });
